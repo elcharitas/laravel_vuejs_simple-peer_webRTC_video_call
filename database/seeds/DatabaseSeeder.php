472747860a48650e92a67908f992262caa8356d4
elcharitas <jonathanirhodia@gmail.com>
2021-11-20 21:46:33 +0100

    chore: add new seed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,8 +13,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         User::create([
-            'name' => 'admin',
-            'email' => 'me@example.com',
+            'name' => 'admin2',
+            'email' => 'me2@example.com',
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi'
         ]);
     }
